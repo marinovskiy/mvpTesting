ff9f16b92929d154edf6259f6e0fb35bfe4fd16e
Александр Мариновский < >
2017-08-07 17:46:18 +0300

    end first test

--- app/src/test/java/com/alex/mvptesting/notes/NotesPresenterTest.java
+++ app/src/test/java/com/alex/mvptesting/notes/NotesPresenterTest.java
@@ -1,32 +1,30 @@
 package com.alex.mvptesting.notes;
 
 import com.alex.mvptesting.addnote.ImmediateSchedulerRule;
+import com.alex.mvptesting.entities.Note;
 import com.alex.mvptesting.model.NoteRepository;
+import com.google.common.collect.Lists;
 
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Collections;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
 
 import io.reactivex.Flowable;
-import io.reactivex.Scheduler;
-import io.reactivex.android.plugins.RxAndroidPlugins;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.Function;
-import io.reactivex.internal.schedulers.ExecutorScheduler;
-import io.reactivex.plugins.RxJavaPlugins;
-import io.reactivex.schedulers.Schedulers;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class NotesPresenterTest {
+
+    private static List<Note> NOTES = Lists.newArrayList(
+            new Note("Title1", "Description1"),
+            new Note("Title2", "Description2")
+    );
 
     @Mock
     private NotesContract.View notesView;
@@ -47,12 +45,11 @@ public class NotesPresenterTest {
 
     @Test
     public void loadNotesFromRepositoryAndLoadIntoView() {
-        when(noteRepository.getAllNotes()).thenReturn(Flowable.just(Collections.emptyList()));
+        when(noteRepository.getAllNotes()).thenReturn(Flowable.just(NOTES));
 
         notesPresenter.loadNotes();
 
         verify(noteRepository).getAllNotes();
-//        verify(notesView).showNotes();
+        verify(notesView).showNotes(NOTES);
     }
-
 }
